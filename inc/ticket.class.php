185eeaf44167e2f5d9acbe0a435d84fecbf8fc7b
Marcio Fernandes <marcio.fernandes@verdanatech.com>
2025-07-11 11:17:20 -0300

    Correction Ticket Add

--- inc/ticket.class.php
+++ inc/ticket.class.php
@@ -841,8 +841,8 @@ class PluginBehaviorsTicket
 
         if (
             $config->getField('use_assign_user_group')
-            && isset($ticket->input['_users_id_assign'])
-            && (count($ticket->input['_users_id_assign']) > 0)
+            && isset($ticket->input['_users_id_assign']) 
+            && (is_array($ticket->input['_users_id_assign']) && count($ticket->input['_users_id_assign']) > 0)
             && (!isset($ticket->input['_groups_id_assign'])
                 || ($ticket->input['_groups_id_assign'] <= 0))
         ) {
